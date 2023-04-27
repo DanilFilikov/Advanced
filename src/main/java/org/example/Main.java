65fd3125ba64dc49863faef8a32d63b172b89c31
DanilFilikov <d.filikov@tg.dunice.net>
2023-04-27 12:48:02 +0300

    refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,27 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import org.example.domain.Collections;
+import org.example.domain.Students;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-       
-        }
+
+        Collections collections = new Collections();
+        System.out.println(collections.getCollection(new ArrayList<>()));
+        collections.getIterator();
+        System.out.println(collections.getNumberoftwo(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}));
+        System.out.println(collections.compareString("ABCDEFDefd", "ABCFEDDedf"));
+        System.out.println(collections.compressBad("AAABBBBBBLLLDCCCDSASfffssssssssssssaaaaaaaaaa "));
+        System.out.println(collections.getChar("aaabsssssffffff"));
+        System.out.println(collections.validateString("a((s)dv)sx"));
+        System.out.println(collections.addBrackets(new StringBuilder("Miserable")));
+        System.out.println(collections.mergeSpace("         asdasd    asd           asd                 das      asd         "));
+        System.out.println(collections.countCouple(new int[]{1,3,5,3,6,7,3,2,1,8,5}));
+
+
     }
 }
